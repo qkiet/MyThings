77f311ecad08dd8577401a3e58904d95535769cc
Tin Tin <qkiet97@gmail.com>
2020-03-20 21:25:33 +0700

    add password entry to entry page, so that user can enter password

--- MyThings/MainPage.xaml.cs
+++ MyThings/MainPage.xaml.cs
@@ -40,7 +40,7 @@ namespace MyThings
                 {
                     cts.CancelAfter(2800);
                     RESTService api_client = new RESTService();
-                    string user_profile = await api_client.Get("https://api.thingspeak.com/channels.json?api_key=" + API_text.Text);
+                    string user_profile = await api_client.Get("https://api.thingspeak.com/channels.json?api_key=" + username_text.Text);
 
                     var settings = new JsonSerializerSettings
                     {
@@ -56,7 +56,7 @@ namespace MyThings
                     {
                         status.TextColor = Color.Green;
                         status.Text = "Thành công!";
-                        API_text.Text = "";
+                        username_text.Text = "";
                         List<UserProfile> profile_json = JsonConvert.DeserializeObject<List<UserProfile>>(user_profile, settings);
                         await Navigation.PushAsync(new Page1(profile_json));
                     }
